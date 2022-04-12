2f192820057dc29a2e65bcff381fbc4335d2d388
matthislm <matthislm@hotmail.com>
2022-04-12 22:47:16 +0200

    sequelize sqlite3

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
