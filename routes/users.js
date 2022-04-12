0ba1dcf41f2bb3aaaed13edf8b28f054e8986963
matthislm <matthislm@hotmail.com>
2022-04-12 23:59:10 +0200

    update user model

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
 
@@ -8,8 +9,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/create',function(req,res) {
-  res.send('ok');
+router.post('/create', async function (req,res) {
+  try{
+    const newUser = await User.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: req.body.password
+    })
+    res.send('ok');
+  } catch(error) {
+    res.statuscode(500).send(':(');
+  }
 });
 
 module.exports = router;
